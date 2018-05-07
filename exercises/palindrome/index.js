a547dca242008c2c884023c33bb71ee62a7cb020
Joanna <jmgsese@gmail.com>
2018-05-07 13:57:27 -0500

    Add their solution

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -15,8 +15,15 @@ function palindrome(str) {
 
 // MY SOLUTION #2
 function palindrome(str) {
-  let reversed = str.split('').reduce((rev, char) => char + rev, '');
+  const reversed = str.split('').reduce((rev, char) => char + rev, '');
   return str === reversed;
+}
+
+// THEIR SOLUTION
+function palindrome(str) {
+  return str.split('').every((char, i) => {
+    return char === str[str.length-i-1]
+  });
 }
 
 module.exports = palindrome;

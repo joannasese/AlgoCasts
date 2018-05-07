3af204e3f3f9f50cf3b9cb3403c014ea3eb67327
Joanna <jmgsese@gmail.com>
2018-05-07 12:49:32 -0500

    Solve reversestring #2

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,8 +6,21 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
+// MY SOLUTION #1
 function reverse(str) {
   return str.split('').reverse().join('');
 }
+
+// MY SOLUTION #2
+function reverse(str) {
+  let strAsArr = str.split('')
+  let reversed = [];
+  for (let i = strAsArr.length-1; i >= 0; i--){
+    reversed.push(strAsArr[i])
+  }
+  return reversed.join('')
+}
+
+//
 
 module.exports = reverse;

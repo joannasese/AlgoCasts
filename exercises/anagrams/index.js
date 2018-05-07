a15aa4716cc2de0370f466bbadc18a14132c881d
Joanna <jmgsese@gmail.com>
2018-05-07 17:41:53 -0500

    Add their solution

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -18,8 +18,32 @@ function anagrams(stringA, stringB) {
 // MY SOLUTION #2
 function anagrams(stringA, stringB) {
   let sorted = (str) => str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
-
   return sorted(stringA) === sorted(stringB);
+}
+
+// THEIR SOLUTION #1
+function anagrams(stringA, stringB) {
+  const aCharMap = buildCharMap(stringA);
+  const bCharMap = buildCharMap(stringB);
+
+  if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length){
+    return false;
+  }
+
+  for (let char in aCharMap) {
+    if (aCharMap[char] !== bCharMap[char]){
+      return false;
+    }
+  }
+  return true;
+}
+
+function buildCharMap(str) {
+  const charMap = {};
+  for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+    charMap[char] = charMap[char] + 1 || 1
+  }
+  return charMap
 }
 
 module.exports = anagrams;

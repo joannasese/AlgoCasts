91d401602e277646568cc0dd578815b8d9302d16
Joanna <jmgsese@gmail.com>
2018-05-07 13:03:25 -0500

    Add their solutions

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -21,6 +21,26 @@ function reverse(str) {
   return reversed.join('')
 }
 
-//
+// THEIR SOLUTION #1
+function reverse(str) {
+  const arr = str.split('');
+  arr.reverse();
+  return arr.join('');
+}
+
+// THEIR SOLUTION #2
+function reverse(str) {
+  let reversed = '';
+
+  for (let character of str){
+    reversed = character + reversed;
+  }
+  return reversed;
+}
+
+// THEIR SOLUTION #3
+function reverse(str) {
+  return str.split('').reduce((rev, char) => char + rev, '')
+}
 
 module.exports = reverse;

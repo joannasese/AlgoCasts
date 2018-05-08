4a37614a052125c8e5807acadc9336702e9d2b71
Joanna <jmgsese@gmail.com>
2018-05-07 23:49:38 -0500

    Solve vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,23 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+// MY SOLUTION #1
+function vowels(str) {
+  let all = str.toLowerCase().match(/[aeiou]/g);
+  return all === null ? 0 : all.length;
+}
+
+// ITERATIVE SOLUTION
+function vowels(str) {
+  let count = 0;
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+  for (let char of str.toLowerCase()){
+    if (vowels.includes(char)){
+      count++
+    }
+  }
+  return count;
+}
 
 module.exports = vowels;

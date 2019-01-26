301e8b7e52d38fa3f60468aec9a2691dcd0d9397
Joanna Sese <jmgsese@gmail.com>
2019-01-26 14:34:05 -0600

    Solve reversestring

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,8 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
-
+// my version
+function reverse(str) {
+    return str.split('').reverse().join('')
+}
 module.exports = reverse;

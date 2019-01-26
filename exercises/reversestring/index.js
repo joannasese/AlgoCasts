e07cc79bd3f8a406b344613eff41dacb03a4577e
Joanna Sese <jmgsese@gmail.com>
2019-01-26 14:59:39 -0600

    Add more solutions

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -8,6 +8,40 @@
 
 // my version
 function reverse(str) {
-    return str.split('').reverse().join('')
+    return str.split('').reverse().join('');
 }
+
+
+// solution 1
+// same as mine
+
+// another solution
+function reverse(str) {
+  let reversed = '';
+  for (let i = str.length-1; i >= 0; i--){
+    reversed += str[i];
+  }
+  return reversed;
+}
+
+// solution 2
+function reverse(str) {
+  // str = back
+  let reversed = '';
+  for (let char of str) {
+    reversed = char + reversed;
+    // first pass: reversed = 'b'
+    // second pass: reversed = 'ab'
+    // third pass: reversed = 'cab'
+    // fourth pass: reversed = 'kcab'
+  }
+  return reversed;
+}
+
+// solution 3
+function reverse(str) {
+  let arr = str.split('');
+  return arr.reduce((reversed, char) => char + reversed, '');
+}
+
 module.exports = reverse;

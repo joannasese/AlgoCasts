5a60b9f06e3576793d07c0a64ed177d21332b020
Joanna Sese <jmgsese@gmail.com>
2019-01-27 08:24:41 -0600

    Add more solutions

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -8,9 +8,43 @@
 // my solution
 function maxChar(str) {
   return str.split('').sort((a,b) =>
-    str.split('')
-    .filter(x => x===a).length - str.split('').filter(x => x===b).length
+    str.split('').filter(x => x===a).length - str.split('').filter(x => x===b).length
   ).pop()
+}
+
+// another solution
+function maxChar(str) {
+  let chars = {};
+  for (let char of str) {
+    chars[char] = chars[char] + 1 || 1;
+  }
+  // return list of keys
+  // sort list in ascending order by value
+  // pop off and return last key
+  return Object.keys(chars).sort((a,b)=>chars[a]-chars[b]).pop();
+}
+
+// solution 1
+function maxChar(str) {
+  const charMap = {};
+  let max = 0;
+  let maxChar = '';
+
+  for (let char of str) {
+    // if/else logic is the same as 'chars[char] = chars[char] + 1 || 1;'
+    if (charMap[char]) {
+      charMap[char]++
+    } else {
+      charMap[char] = 1
+    }
+  }
+  for (let char in charMap) {
+    if (charMap[char] > max){
+      max = charMap[char]
+      maxChar = char;
+    }
+  }
+  return maxChar;
 }
 
 module.exports = maxChar;

f9548dd7ba365ef082643762f230cf3a80b35e5e
Joanna Sese <jmgsese@gmail.com>
2019-01-27 20:22:24 -0600

    Solve anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,8 +8,36 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
+// my solution
 function anagrams(stringA, stringB) {
-  //test test
+  let simplify = (str) => str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+  return simplify(stringA) === simplify(stringB);
+}
+
+// solution 1
+function anagrams(stringA, stringB) {
+  const aCharMap = buildCharMap(stringA);
+  const bCharMap = buildCharMap(stringB);
+
+  if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+    return false;
+  }
+
+  for (let char in aCharMap) {
+    if (aCharMap[char] !== bCharMap[char]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function buildCharMap(str) {
+  const charMap = {};
+
+  for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+  return charMap;
 }
 
 module.exports = anagrams;
